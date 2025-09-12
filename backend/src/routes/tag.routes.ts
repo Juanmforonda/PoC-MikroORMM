b7e7eabe2cbb63c1777b2bba82cf55d33e279c7e
Juanmforonda <juanmforonda@hotmail.com>
2025-09-12 15:50:37 -0300

    carpeta backend terminada (creo)

---
+++ backend/src/routes/tag.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { findAll, remove, findOne, add, update } from "../controllers/tag.controller.js";
+
+const tagRouter = Router();
+
+tagRouter.get('/', findAll);
+tagRouter.get('/:id', findOne);
+tagRouter.post('/', add);
+tagRouter.put('/:id', update);
+tagRouter.delete('/:id', remove);
+
+export default tagRouter;
